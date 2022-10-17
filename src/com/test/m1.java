21d9fa993324f9ace7e7d8a6cca84019b4d9553c
mani_12 <saravanamani002@gamil.com>
2022-10-17 23:18:28 +0530

    new

--- src/com/test/m1.java
+++ src/com/test/m1.java
@@ -4,12 +4,15 @@ public class m1 {
 
 	public static void main(String[] args) {
 
-
 		
 		System.out.println("hi");
 		System.out.println("this is messi");
 		System.out.println("im a football player");
-		
+		System.out.println("onnum illa");
+		System.out.println("im from argentina");
+		System.out.println("hi");
+		System.out.println("this is messi");
+		System.out.println("im a football player");
 		System.out.println("onnum illa");
 		System.out.println("im from argentina");
 	}
